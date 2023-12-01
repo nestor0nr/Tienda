d341fc326b993df56feb1d8e16c609a3dad517b1
Np <nestpin@hotmail.es>
2023-12-01 09:31:24 -0600

    Finish Country CRUD

---
+++ Tienda/Tienda/Data/Entities/Country.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tienda.Data.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name="Pais")]
+        [MaxLength(50,ErrorMessage ="El campo {0} debe de tener maximo {1} caracter.")]
+        [Required(ErrorMessage ="El campo {0} es obligatorio")]
+        public string? Name { get; set; }
+
+    }
+}
